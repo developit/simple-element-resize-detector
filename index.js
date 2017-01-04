c080e01a1e165f80a22fb4ca1bdef76bc60556e5
Jason Miller <jasonmiller@synacor.com>
2017-01-04 00:04:09 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const CSS = 'position:absolute;left:0;top:-100%;width:100%;height:100%;margin:1px 0 0;border:none;opacity:0;visibility:hidden;pointer-events:none;';
+
+export default (element, handler) => {
+	let frame = document.createElement('iframe');
+	frame.style.cssText = CSS;
+	element.appendChild(frame);
+	frame.contentWindow.onresize = () => {
+		handler.call(element);
+	};
+	return frame;
+}
