2861341e6aa928f080580e29bd671f114d6f13e9
Arthur Stolyar <nekr.fabula@gmail.com>
2017-10-27 22:22:39 +0300

    Move pointerEvents check to the exported function

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-let CSS = 'position:absolute;left:0;top:-100%;width:100%;height:100%;margin:1px 0 0;border:none;opacity:0;pointer-events:none;';
-const PE = document.documentMode < 11 ? false : 'pointerEvents' in document.documentElement;
-
-if (!PE) {
-  CSS += 'visibility:hidden;';
-}
+const CSS = 'position:absolute;left:0;top:-100%;width:100%;height:100%;margin:1px 0 0;border:none;opacity:0;pointer-events:none;';
 
 export default (element, handler) => {
 	let frame = document.createElement('iframe');
-	frame.style.cssText = CSS;
+  let supportsPE = document.documentMode < 11 ? false : 'pointerEvents' in frame.style;
+
+	frame.style.cssText = supportsPE ? CSS : CSS + 'visibility:hidden;';
 	element.appendChild(frame);
 	frame.contentWindow.onresize = () => { handler(element); };
 	return frame;
