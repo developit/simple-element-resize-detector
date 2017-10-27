6d58313c25f144eba5d942611469f61a6c484a64
Arthur Stolyar <nekr.fabula@gmail.com>
2017-10-27 22:06:42 +0300

    Fix Firefox visiblity issue

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const CSS = 'position:absolute;left:0;top:-100%;width:100%;height:100%;margin:1px 0 0;border:none;opacity:0;visibility:hidden;pointer-events:none;';
+const PE = document.documentMode < 11 ? false : 'pointerEvents' in style;
+let CSS = 'position:absolute;left:0;top:-100%;width:100%;height:100%;margin:1px 0 0;border:none;opacity:0;pointer-events:none;';
+
+if (!PE) {
+  CSS += 'visibility:hidden;';
+}
 
 export default (element, handler) => {
 	let frame = document.createElement('iframe');
