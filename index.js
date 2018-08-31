791db38792ab891052e1e0d88fe04c4518cf6f3c
Jason Miller <developit@google.com>
2018-08-31 10:06:10 -0400

    avoid unnecessary returns

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const CSS = 'position:absolute;left:0;top:-100%;width:100%;height:100%;margin:1p
 export default (element, handler) => {
 	let frame = document.createElement('iframe');
 	frame.style.cssText = CSS;
-	frame.onload = () => frame.contentWindow.onresize = () => handler(element);
+	frame.onload = () => {
+		frame.contentWindow.onresize = () => {
+			handler(element);
+		};
+	};
 	element.appendChild(frame);
 	return frame;
 }
