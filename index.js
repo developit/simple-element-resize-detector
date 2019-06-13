b9ce0983d8253e22240f7113244eadc06c694b28
Jason Miller <developit@google.com>
2019-06-13 09:01:15 -0400

    Reduce size

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const CSS = 'position:absolute;left:0;top:-100%;width:100%;height:100%;margin:1px 0 0;border:none;opacity:0;pointer-events:none;';
+const CSS = `position:absolute;left:0;top:-100%;width:100%;height:100%;margin:1px 0 0;border:none;opacity:0;pointer-events:none;`;
 
 export default (element, handler) => {
-	let frame = document.createElement('iframe');
-	let supportsPE = document.documentMode < 11 ? false : 'pointerEvents' in frame.style;
+	const frame = document.createElement('iframe');
+	const supportsPE = document.documentMode < 11 && 'pointerEvents' in frame.style;
 
-	frame.style.cssText = supportsPE ? CSS : CSS + 'visibility:hidden;';
+	frame.style.cssText = `${CSS}${supportsPE ? '' : 'visibility:hidden;'}`;
 	frame.onload = () => {
 		frame.contentWindow.onresize = () => {
 			handler(element);
@@ -12,4 +12,4 @@ export default (element, handler) => {
 	};
 	element.appendChild(frame);
 	return frame;
-}
+};
